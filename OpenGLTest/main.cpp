2172e5b65a6d27c7332a1349367388831df62844
sungminkim <ksm3169@gmail.com>
2020-03-07 01:05:33 +0900

    Initial Commit

---
+++ OpenGLTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLTest
+//
+//  Created by 김성민 on 2020/03/07.
+//  Copyright © 2020 Sung Min Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
